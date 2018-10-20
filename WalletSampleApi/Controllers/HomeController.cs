afd651f45983a97fcd4727b805d7edb1fbac5ff8
Teerachai Laothong <tlaothong@gmail.com>
2018-10-20 15:23:51 +0700

    Simple Test is quite OK

--- WalletSampleApi/Controllers/HomeController.cs
+++ WalletSampleApi/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using WalletSampleApi.Models;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace WalletSampleApi.Controllers
@@ -18,30 +20,67 @@ namespace WalletSampleApi.Controllers
             return View();
         }
 
-        private static readonly string[] coins =
-        new [] {
-            "BTC",
-            "ETH",
-            "XRP",
-            "BCH",
-            "EOS",
-            "XLM",
-            "LTC",
-            "USDT",
-            "ADA",
-            "XMR",
-            "TRX",
-            "DASH",
-            "BNB",
-            "NEO",
-            "ETC",
-            "XEM",
+        private static readonly Dictionary<string, double> coins =
+        new Dictionary<string, double> {
+            { "BTC", 6488 },
+            { "ETH", 205 },
+            { "XRP", 0.46 },
+            { "BCH", 443 },
+            { "EOS", 5.4 },
+            { "XLM", 0.24 },
+            { "LTC", 53.25 },
+            { "USDT", 0.98 },
+            { "ADA", 0.076 },
+            { "XMR", 104 },
+            { "TRX", 0.024 },
+            { "DASH", 155 },
+            { "BNB", 9.76 },
+            { "NEO", 16.54 },
+            { "ETC", 9.66 },
+            { "XEM", 0.094 },
         };
 
         [HttpPost]
-        public IActionResult SimpleTest(string url)
+        public async Task<IActionResult> SimpleTest(string url, bool all)
         {
-            return Content(string.Join(',', coins));
+            var clen = coins.Count;
+            var coinList = new List<(string, double)>();
+            var rdm = new Random();
+
+            foreach (var c in coins)
+            {
+                if (all || rdm.Next(10) < 7)
+                {
+                    coinList.Add((c.Key, c.Value));
+                }
+            }
+
+            var priceUpdate = new CoinPriceUpdate
+            {
+                At = DateTime.Now,
+                PriceList = new List<CoinPrice>(),
+            };
+
+            var plist = priceUpdate.PriceList;
+
+            foreach (var (sym,  pri) in coinList)
+            {
+                var buy = pri + (rdm.NextDouble() - 0.5) * 0.24 * pri;
+                var sell = buy - Math.Max(0.03, rdm.NextDouble() * 0.12) * pri;
+                plist.Add(new CoinPrice
+                {
+                    Symbol = sym,
+                    Buy = buy,
+                    Sell = sell,
+                });
+            }
+
+            var hcli = new HttpClient();
+            await hcli.PostAsJsonAsync<CoinPriceUpdate>(url, priceUpdate);
+
+            ViewData["url"] = url;
+
+            return View();
         }
     }
 }
